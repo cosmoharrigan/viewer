aca5460c7f3a12ea327c82be8741010f5e008ed3
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2014-01-06 01:08:19 -0800

    Works

--- web.js
+++ web.js
@@ -1,15 +1,11 @@
-// web.js
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
 var app = express();
 
-app.use(logfmt.requestLogger());
+app.use(express.static('public'))
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World');
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(3000);
+console.log('Listening on port 3000');
