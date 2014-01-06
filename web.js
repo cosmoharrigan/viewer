cdc27801659da870b9acb2701793935062da9081
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2014-01-06 01:25:20 -0800

    Remove unnecessary test

--- web.js
+++ web.js
@@ -3,9 +3,5 @@ var app = express();
 
 app.use(express.static('public'))
 
-app.get('/hello.txt', function(req, res){
-  res.send('Hello World');
-});
-
-app.listen(process.env.PORT || 5000);
-console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000);
+console.log('Listening...');
